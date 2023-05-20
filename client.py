85ea368ced3dcd9ec1e0c2299098deacdbcf262b
Cody Lewis <hello@codymlewis.com>
2023-05-20 10:48:21 +1000

    Added norm layer to FL example

--- client.py
+++ client.py
@@ -18,7 +18,9 @@ def create_model(sample_shape: typing.Tuple[int]) -> tf.keras.Model:
     """
     inputs = tf.keras.Input(sample_shape)
     x = tf.keras.layers.Dense(100, activation="relu")(inputs)
+    x = tf.keras.layers.LayerNormalization()(x)
     x = tf.keras.layers.Dense(50, activation="relu")(x)
+    x = tf.keras.layers.LayerNormalization()(x)
     x = tf.keras.layers.Dense(1, activation="sigmoid")(x)
     model = tf.keras.Model(inputs=inputs, outputs=x)
     model.compile(
